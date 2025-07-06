ccee40d7cef7a22afff63f0ba4acaf8cbbf97e45
Joaquin Hernandez <joaquinhernandezph@gmail.com>
2025-07-05 21:21:52 -0300

    Reset scroll position on pagination change

--- src/views/Patients/PatientsGrid.tsx
+++ src/views/Patients/PatientsGrid.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import PatientCard from "../../components/patients/PatientCard";
 import type { Patient } from "../../components/patients/types";
 import DataStateMessage from "../../components/shared/DataStateMessage";
@@ -7,12 +8,25 @@ interface Props {
 }
 
 export default function PatientsGrid({ patients }: Props) {
+  const gridContainerRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    // Reset grid scroll-Y position on pagination change
+    const el = gridContainerRef.current;
+    if (el) {
+      el.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [patients]);
+
   if (!patients.length) {
     return <DataStateMessage title="No patients found" description="No patients were found." type="empty" />;
   }
 
   return (
-    <section className="grid h-full w-full grid-cols-[repeat(auto-fill,minmax(250px,1fr))] content-start gap-3 overflow-y-auto pr-3 pb-3">
+    <section
+      ref={gridContainerRef}
+      className="grid h-full w-full grid-cols-[repeat(auto-fill,minmax(250px,1fr))] content-start gap-3 overflow-y-auto pr-3 pb-3"
+    >
       {patients.map(({ id, name, description, avatar, website }) => (
         <PatientCard key={id} name={name} avatar={avatar} website={website} description={description} />
       ))}
